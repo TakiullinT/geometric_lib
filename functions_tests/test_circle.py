c2963e66fbb7edc524d7bf6ba27806c4d172a50e
Takiullin Timur <timurtakiullin@icloud.com>
2024-11-21 21:50:16 +0300

    Update test_circle.py

--- functions_tests/test_circle.py
+++ functions_tests/test_circle.py
@@ -2,40 +2,29 @@ import unittest
 import math
 from circle import area, perimeter
 
-class testing_circle_function(unittest.TestCase):
 
-    def testing_the_zero_area(self):
+class TestingCircleFunction(unittest.TestCase):
+
+    def test_the_zero_area(self):
         the_radius = 0
-
         result_of_the_actions = area(the_radius)
-        
         self.assertEqual(result_of_the_actions, 0)
 
-    def testing_the_zero_perimeter(self):
+    def test_the_zero_perimeter(self):
         the_radius = 0
-
         result_of_the_actions = perimeter(the_radius)
-
         self.assertEqual(result_of_the_actions, 0)
 
-    def testing_the_preimeter_actions(self):
+    def test_the_perimeter_actions(self):
         the_radius = 1
-
         result_of_the_actions = perimeter(the_radius)
-
         self.assertEqual(result_of_the_actions, 2 * math.pi)
-    
-    def testing_the_area_actions(self):
+
+    def test_the_area_actions(self):
         the_radius = 1
-
         result_of_the_actions = area(the_radius)
-
         self.assertEqual(result_of_the_actions, math.pi)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
