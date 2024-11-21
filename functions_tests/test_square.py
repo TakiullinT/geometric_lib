8e13014e55798bb7a67db5793dc841ee3ce475bf
Takiullin Timur <timurtakiullin@icloud.com>
2024-11-21 21:12:29 +0300

    Update test_square.py

--- functions_tests/test_square.py
+++ functions_tests/test_square.py
@@ -1,41 +1,29 @@
 import unittest
-import math
 from square import perimeter, area
 
-class testing_square_function(unittest.TestCase):
 
-    def testing_the_zero_area(self):
+class TestingSquareFunction(unittest.TestCase):
+
+    def test_the_zero_area(self):
         the_side = 0
-
         result_of_the_actions = area(the_side)
-
         self.assertEqual(result_of_the_actions, 0)
-    
-    def testing_the_zero_perimeter(self):
-        the_side = 0
 
+    def test_the_zero_perimeter(self):
+        the_side = 0
         result_of_the_actions = perimeter(the_side)
-
         self.assertEqual(result_of_the_actions, 0)
 
-    def testing_the_area_actions(self):
+    def test_the_area_actions(self):
         the_side = 1
-
         result_of_the_actions = area(the_side)
-
         self.assertEqual(result_of_the_actions, 1)
 
-    def testing_the_perimeter_actions(self):
+    def test_the_perimeter_actions(self):
         the_side = 1
-
         result_of_the_actions = perimeter(the_side)
-
         self.assertEqual(result_of_the_actions, 4)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
