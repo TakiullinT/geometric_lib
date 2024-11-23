f159e7edfb879bdd99407a581627b4551073b365
Takiullin Timur <timurtakiullin@icloud.com>
2024-11-23 14:21:48 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -25,10 +25,6 @@ def calc(fig, func, size):
     assert len(size) == expected_size
     assert all(s >= 0 for s in size)
 
-    if fig == 'triangle':
-        a, b, c = size
-        assert a + b > c and a + c > b and b + c > a
-
     result = eval(
         f'{fig}.{func}(*{size})',
         {"circle": circle, "square": square, "triangle": triangle}
