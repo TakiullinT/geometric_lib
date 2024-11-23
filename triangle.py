8a50bb9d9a7906e0e3fca374602c52f7db6f4676
Takiullin Timur <timurtakiullin@icloud.com>
2024-11-23 17:14:08 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -3,8 +3,6 @@ def area(a, b, c):
         return 0
     assert a + b > c and a + c > b and b + c > a
     assert all(s >= 0 for s in (a, b, c))
-    expected_size = 3
-    assert len([a, b, c]) == expected_size
     p = (a + b + c) / 2
     return (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
