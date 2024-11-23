fba2f00c3fdcd422e98bfe1908143933ae5e5179
Takiullin Timur <timurtakiullin@icloud.com>
2024-11-23 14:10:33 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,12 @@
 def area(a, b, c):
+    expected_size = 3
+    assert a + b > c and a + c > b and b + c > a
     p = (a + b + c) / 2
+    assert len([a, b, c]) == expected_size
+    assert all(s >= 0 for s in (a, b, c))
     return (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
 
 def perimeter(a, b, c):
+    assert a + b > c and a + c > b and b + c > a
     return a + b + c
